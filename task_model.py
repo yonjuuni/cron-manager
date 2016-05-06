a98676a3031e6903dc86e7f286a13cd7488e14f2
zlyu88 <zlyu88@gmail.com>
2016-05-06 10:45:24 +0300

    Edit task model

--- task_model.py
+++ task_model.py
@@ -18,114 +18,151 @@ class Task(object):
         self.reboot = reboot
 
     def __check_minute(self, minute):
-        if ',' in minute:
-            minute = minute.split(',')
+        slash = [0, 0]
+        if '/' in minute:
+            slash = minute.split('/')
+            if int(slash[1]) not in range (1, 60):
+                print "Wrong using of '/' for minute"
+                slash[1] = 0
+        else:
+            slash[0] = minute    
+
+        if ',' in slash[0]:
+            slash[0] = slash[0].split(',')
             separator = ','
         elif '-' in minute:
-            minute = minute.split('-')
+            slash[0] = slash[0].split('-')
             separator = '-'
-        elif minute == '*':
-            return minute
-        elif  '*/' in minute:
-            test = minute.split('/')
-            if int(test[1]) not in range(2, 31):
-                print "Wrong using of '/' for minute"
-                minute = '*'
+        elif slash[0] == '*':
+            if slash[1]:
                 return minute
             else:
-                return minute
+                return '*'
         else:
+            if slash[1]:
+                print "Wrong using of '/' for minute"
+                slash[1] = 0
             separator = None
 
-        if isinstance(minute, list):
-            for unit in minute:
+        if isinstance(slash[0], list):
+            for unit in slash[0]:
                 if int(unit) not in range(0, 60):
                     print 'minute not in range'
                     if separator == ',':
-                        minute[minute.index(unit)] = '59'
+                        slash[0][slash[0].index(unit)] = '59'
                     elif separator == '-':
-                        minute[minute.index(unit)] = '59'
-                        minute = minute[:minute.index('59') + 1]
-                        minute = separator.join(minute)
-                        return minute
+                        slash[0][slash[0].index(unit)] = '59'
+                        slash[0] = slash[0][:slash[0].index('59') + 1]
+                        #slash[0] = separator.join(slash[0])
         else:
-            if int(minute) not in range(0, 60):
+            if int(slash[0]) not in range(0, 60):
                 print 'minute not in range'
-                minute = '59'
-                return minute
+                slash[0] = '59'
         
-        if separator:
-            minute = sorted(set(minute))
-            minute = separator.join(minute)
-            return minute
+        if separator == ',':
+            slash[0] = sorted(set(slash[0]))
+            slash[0] = separator.join(slash[0])
+        elif separator == '-':
+            slash[0] = separator.join(slash[0])
+
+        if slash[1]:
+            minute = '/'.join(slash)
+        else:
+            minute = slash[0]
         return minute
 
     def __check_hour(self, hour):
-        if ',' in hour:
-            hour = hour.split(',')
-            separator = ','
-        elif '-' in hour:
-            hour = hour.split('-')
-            separator = '-'
-        elif hour == '*':
-            return hour
-        elif  '*/' in hour:
-            test = hour.split('/')
-            if int(test[1]) not in range(2, 13):
+        slash = [0, 0]
+        if '/' in hour:
+            slash = hour.split('/')
+            if int(slash[1]) not in range (1, 24):
                 print "Wrong using of '/' for hour"
-                hour = '*'
+                slash[1] = 0
+        else:
+            slash[0] = hour    
+
+        if ',' in slash[0]:
+            slash[0] = slash[0].split(',')
+            separator = ','
+        elif '-' in slash[0]:
+            slash[0] = slash[0].split('-')
+            separator = '-'
+        elif slash[0] == '*':
+            if slash[1]:
                 return hour
             else:
-                return hour
+                return '*'
         else:
+            if slash[1]:
+                print "Wrong using of '/' for hour"
+                slash[1] = 0
             separator = None
 
-        if isinstance(hour, list):
-            for unit in hour:
+        if isinstance(slash[0], list):
+            for unit in slash[0]:
                 if int(unit) not in range(0, 24):
                     print 'hour not in range'
                     if separator == ',':
-                        hour[hour.index(unit)] = '0'
+                        slash[0][slash[0].index(unit)] = '0'
                     elif separator == '-':
-                        hour[hour.index(unit)] = '0'
-                        hour = hour[:hour.index('0') + 1]
-                        hour = separator.join(hour)
-                        return hour
+                        slash[0][slash[0].index(unit)] = '0'
+                        slash[0] = slash[0][:slash[0].index('0') + 1]
+                        #slash[0] = separator.join(slash[0])
         else:
-            if int(hour) not in range(0, 24):
+            if int(slash[0]) not in range(0, 24):
                 print 'hour not in range'
-                hour = '*'
-                return hour
+                slash[0] = '0'
 
-        if separator:
-            hour = sorted(set(hour))
-            hour = separator.join(hour)
-            return hour
+        if separator == ',':
+            slash[0] = sorted(set(slash[0]))
+            slash[0] = separator.join(slash[0])
+        elif separator == '-':
+            slash[0] = separator.join(slash[0])
+            
+        if slash[1]:
+            hour = '/'.join(slash)
+        else:
+            hour = slash[0]
         return hour
 
     def __check_date(self):
         max_value = []
-        if self.day_of_month != '*' and self.month != '*':
+        slash_day = [0, 0]
+        slash_month = [0, 0]
 
-            if ',' in self.day_of_month:
-                all_days_of_month = self.day_of_month.split(',')
+        if '/' in self.day_of_month:
+            slash_day = self.day_of_month.split('/')
+        else:
+            slash_day[0] = self.day_of_month
+
+        if '/' in self.month:
+            slash_month = self.month.split('/')
+        else:
+            slash_month[0] = self.month
+
+        if slash_day[0] != '*' and slash_month[0] != '*':
+
+            if ',' in slash_day[0]:
+                all_days_of_month = slash_day[0].split(',')
+                max_value.append(all_days_of_month[-1])
                 separator = ','
-            elif '-' in self.day_of_month:
-                day_of_month = self.day_of_month.split('-')
+            elif '-' in slash_day[0]:
+                day_of_month = slash_day[0].split('-')
                 all_days_of_month = range(int(day_of_month[0]),
                                           int(day_of_month[-1]) + 1)
+                max_value.append(all_days_of_month[-1])
                 separator = '-'
             else:
-                all_days_of_month = [self.day_of_month]
+                all_days_of_month = [slash_day[0]]
                 separator = '!'
 
-            if ',' in self.month:
-                all_month = self.month.split(',')
-            elif '-' in self.month:
-                month = self.month.split('-')
+            if ',' in slash_month[0]:
+                all_month = slash_month[0].split(',')
+            elif '-' in slash_month[0]:
+                month = slash_month[0].split('-')
                 all_month = range(int(month[0]), int(month[-1]) + 1)
             else:
-                all_month = [self.month]
+                all_month = [slash_month[0]]
 
             for month in all_month:
                 for day in all_days_of_month:
@@ -150,60 +187,77 @@ class Task(object):
                 for day in all_days_of_month:
                     if day <= max_value:
                         new_day_of_month.append(day)
-                return separator.join(new_day_of_month)
+                new_days = separator.join(new_day_of_month)
             elif separator == '-':
                 if day_of_month[0] == max_value:
                     day_of_month = [max_value]
                 else:
                     day_of_month[1] = max_value
-                return separator.join(day_of_month)
+                new_days = separator.join(day_of_month)
             else:
                 if int(all_days_of_month[0]) > int(max_value):
-                    return max_value
+                    new_days =  max_value
                 else:
-                    return all_days_of_month[0]
+                    new_days = all_days_of_month[0]
 
-        return self.day_of_month
+            slash_day[0] = new_days       
+
+        if slash_day[1]:
+            day_of_month = '/'.join(slash_day)
+        else:
+            day_of_month = slash_day[0]
+        return day_of_month
 
     def __check_day_of_the_week(self, day_of_week):
         week_days = {'mon': '1', 'tue': '2', 'wed': '3', 'thu': '4',
                      'fri': '5', 'sat': '6', 'sun': '0'}
+        slash = [0, 0]
+        if '/' in day_of_week:
+            slash = day_of_week.split('/')
+            if int(slash[1]) not in range (1, 8):
+                print "Wrong using of '/' for day_of_week"
+                slash[1] = 0
+        else:
+            slash[0] = day_of_week 
 
-        if ',' in day_of_week:
-            day_of_week = day_of_week.split(',')
+        if ',' in slash[0]:
+            slash[0] = slash[0].split(',')
             separator = ','
-        elif '-' in day_of_week:
-            day_of_week = day_of_week.split('-')
+        elif '-' in slash[0]:
+            slash[0] = slash[0].split('-')
             separator = '-'
-        elif day_of_week == '*':
-            return day_of_week
-        elif  '*/' in day_of_week:
-            test = day_of_week.split('/')
-            if int(test[1]) not in range(2, 4):
-                print "Wrong using of '/' fo day_of_week"
-                day_of_week = '*'
+        elif slash[0] == '*':
+            if slash[1]:
                 return day_of_week
             else:
-                return day_of_week
+                return '*'
         else:
-            separator = None
+            if slash[1]:
+                print "Wrong using of '/' for day_of_week"
+                slash[1] = 0
+            separator = None        
 
-        if isinstance(day_of_week, list):
-            for unit in day_of_week:
+        if isinstance(slash[0], list):
+            for unit in slash[0]:
                 if unit not in week_days and unit not in week_days.values():
                     print 'day_of_week not in range'
-                    day_of_week = '*'
-                    return day_of_week
+                    slash[0] = '*'
+                    # return day_of_week
         else:
-            for day in week_days:
-                if day_of_week == day or day_of_week == week_days.get(day):
-                    return day_of_week
-            print 'day_of_week not in range'
-            day_of_week = '*'
-            return day_of_week
+            if slash[0] not in week_days and slash[0] not in week_days.values():
+                print 'day_of_week not in range'
+                slash[0] = '7'
 
-        if separator:
-            return separator.join(day_of_week)
+        if separator == ',':
+            slash[0] = sorted(set(slash[0]))
+            slash[0] = separator.join(slash[0])
+        elif separator == '-':
+            slash[0] = separator.join(slash[0])
+            
+        if slash[1]:
+            day_of_week = '/'.join(slash)
+        else:
+            day_of_week = slash[0]
         return day_of_week
 
     def output(self):
@@ -214,84 +268,114 @@ class Task(object):
                + str(self.day_of_week) + ' ' + self.action
 
     def __check_month(self, month):
-        if ',' in month:
-            month = month.split(',')
-            separator = ','
-        elif '-' in month:
-            month = month.split('-')
-            separator = '-'
-        elif month == '*':
-            return month
-        elif  '*/' in month:
-            test = month.split('/')
-            if int(test[1]) not in range(2, 7):
+        slash = [0, 0]
+        if '/' in month:
+            slash = month.split('/')
+            if int(slash[1]) not in range (1, 13):
                 print "Wrong using of '/' for month"
-                month = '*'
+                slash[1] = 0
+        else:
+            slash[0] = month  
+
+        if ',' in slash[0]:
+            slash[0] = slash[0].split(',')
+            separator = ','
+        elif '-' in slash[0]:
+            slash[0] = slash[0].split('-')
+            separator = '-'
+        elif slash[0] == '*':
+            if slash[1]:
                 return month
             else:
-                return month
+                return '*'
         else:
+            if slash[1]:
+                print "Wrong using of '/' for month"
+                slash[1] = 0
             separator = None
 
-        if isinstance(month, list):
-            for unit in month:
+        if isinstance(slash[0], list):
+            for unit in slash[0]:
                 if int(unit) not in range(1, 13):
                     print 'month not in range'
-                    month = '*'
-                    return month
+                    if separator == ',':
+                        slash[0][slash[0].index(unit)] = '12'
+                    elif separator == '-':
+                        slash[0][slash[0].index(unit)] = '12'
+                        slash[0] = slash[0][:slash[0].index('12') + 1]
+                        #slash[0] = separator.join(slash[0])
         else:
-            if int(month) not in range(1, 13):
+            if int(slash[0]) not in range(1, 13):
                 print 'month not in range'
-                month = '*'
-                return month
+                slash[0] = '0'
 
-        if separator:
-            return separator.join(month)
+        if separator == ',':
+            slash[0] = sorted(set(slash[0]))
+            slash[0] = separator.join(slash[0])
+        elif separator == '-':
+            slash[0] = separator.join(slash[0])
+            
+        if slash[1]:
+            month = '/'.join(slash)
+        else:
+            month = slash[0]
         return month
 
     def __check_day_of_month(self, day_of_month):
-        if ',' in day_of_month:
-            day_of_month = day_of_month.split(',')
-            separator = ','
-        elif '-' in day_of_month:
-            day_of_month = day_of_month.split('-')
-            separator = '-'
-        elif day_of_month == '*':
-            return day_of_month
-        elif  '*/' in day_of_month:
-            test = day_of_month.split('/')
-            if int(test[1]) not in range(2, 16):
+        slash = [0, 0]
+        if '/' in day_of_month:
+            slash = day_of_month.split('/')
+            if int(slash[1]) not in range (1, 16):
                 print "Wrong using of '/' for day_of_month"
-                day_of_month = '*'
+                slash[1] = 0
+        else:
+            slash[0] = day_of_month  
+        print 'slash =', slash 
+        if ',' in slash[0]:
+            slash[0] = slash[0].split(',')
+            separator = ','
+        elif '-' in slash[0]:
+            slash[0] = slash[0].split('-')
+            separator = '-'
+        elif slash[0] == '*':
+            if slash[1]:
                 return day_of_month
             else:
-                return day_of_month
+                return '*'
         else:
+            if slash[1]:
+                print "Wrong using of '/' for day_of_month"
+                slash[1] = 0
             separator = None
 
-        if isinstance(day_of_month, list):
-            for unit in day_of_month:
+        if isinstance(slash[0], list):
+            for unit in slash[0]:
                 if int(unit) not in range(1, 32):
                     print 'day_of_month not in range'
                     if separator == ',':
-                        day_of_month[day_of_month.index(unit)] = '30'
+                        slash[0][slash[0].index(unit)] = '31'
                     elif separator == '-':
-                        day_of_month[day_of_month.index(unit)] = '30'
-                        day_of_month = day_of_month[:day_of_month.index('30')+1]
-                        day_of_month = separator.join(day_of_month)
-                        return day_of_month
+                        slash[0][slash[0].index(unit)] = '31'
+                        slash[0] = slash[0][:slash[0].index('31')+1]
+                        # day_of_month = separator.join(day_of_month)
         else:
-            if int(day_of_month) not in range(1, 32):
+            if int(slash[0]) not in range(1, 32):
                 print 'day_of_month not in range'
-                day_of_month = '*'
-                return day_of_month
+                slash[0] = '*'
 
-        if separator:
-            day_of_month = sorted(set(day_of_month))
-            day_of_month = separator.join(day_of_month)
-            return day_of_month
+        if separator == ',':
+            slash[0] = sorted(set(slash[0]))
+            slash[0] = separator.join(slash[0])
+        elif separator == '-':
+            slash[0] = separator.join(slash[0])
+            
+        if slash[1]:
+            day_of_month = '/'.join(slash)
+        else:
+            day_of_month = slash[0]
         return day_of_month
 
 
-# example = Task('*/31', '*/31', '28-31', '2-5', '*/10', 'burn!')
-# print example.output()
+example = Task('1', '1', '10,22/2', '1-13', 'mon', 'burn!')
+print example.output()
+
